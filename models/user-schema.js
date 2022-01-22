13c7e03184bf9766d6960306f802e0f0cbbe9073
khanhnguyenvan3152 <54436766+khanhnguyenvan3152@users.noreply.github.com>
2022-01-22 12:28:49 +0700

    fix user-schema.js

--- models/user-schema.js
+++ models/user-schema.js
@@ -4,4 +4,33 @@ const UserSchema = mongoose.Schema({
     guildId: String,
     balance: Number
 })
+UserSchema.methods.bet = async function (matchId,choice,value){
+    let res;
+    let match = await Match.findById(matchId)
+    if(match){
+        if(match.status == 'completed') res = 'Match is already started'
+        else{
+            let matchId = match._id
+            let rate = (choice=='away')?match.oddAway:match.oddHome
+            let ticket = new Ticket({
+                match: matchId,
+                user:this._id,
+                value: value,
+                side: choice,
+                rate:rate
+            })
+            await ticket.save()
+            match.tickets.push(ticket._id)
+            match.total = match.total + value
+            if(choice=='away'){
+                match.totalAway = match.totalAway + value
+            }else{
+                match.totalHome = match.totalHome + value
+            }
+            await match.save()
+            this.balance = this.balance - value
+            await this.save()
+        }
+    }
+}
 module.exports = mongoose.model('users',UserSchema)
